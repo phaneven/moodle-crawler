a91f42e356dcf95c74983bc3542460a4f0e9346d
Ping Fan <itintern@sdcdom.local>
2017-08-08 15:58:38 +1000

    fetch courses title from UNSW moodle

--- moodle.js
+++ moodle.js
@@ -5,29 +5,46 @@ var casper = require('casper').create({
 var username = 'z5119693';
 var password = 'hyw523974055^&*()';
 var url = 'https://moodle.telt.unsw.edu.au/login/';
+var courses = [];
+
+function getCourses() {
+    var courses = document.querySelectorAll('h2.title a');
+    return Array.prototype.map.call(courses, function(e) {
+        return e.getAttribute('title');
+    });
+}
 
 casper.start();
 casper.thenOpen(url);
 
 casper.then(function(){
     casper.echo('url opened');
-    casper.waitForSelector('iframe', function(){
-        casper.echo('form ready');
-        casper.sendKeys('input#username', username);
-        casper.sendKeys('input#password', password);
-        casper.thenClick('input#submit');
-        casper.echo('button clicked');
-        // casper.waitForSelector('div[class="mobPass"]', function(){
-        //     casper.sendKeys('input#password',password);
-        //     casper.thenClick('input#submit');
-        // }, 10000)
+    casper.waitForSelector('iframe', function () {
+        casper.withFrame(0, function() {
+            casper.sendKeys('input#username', username);
+            casper.sendKeys('input#password', password);
+            casper.thenClick('input#submit');
+            casper.echo('button clicked');
+        });
     }, function () {
-        casper.echo('cannot find login form').die();
+        casper.echo ('cannot load frame').die();
     }, 1000);
+    
+})
+casper.page.settings.webSecurityEnabled = false;
+
+casper.then(function(){
+    casper.echo(this.getTitle());
+    casper.waitForSelector('.course_list', function () {
+        courses = casper.evaluate(getCourses);
+        for (i in courses) {
+            casper.echo(courses[i]);
+        }
+        // casper.echo(courses.length);
+    });
 })
 
-// casper.then(function(){
-//     // console.log('success');
-// })
 
-casper.run();
+casper.run(function () {
+    casper.echo('Done.').exit();
+});
