9f4de1768907e918bd398da86364b53a099a9b5f
Ping Fan <itintern@sdcdom.local>
2017-08-09 17:36:02 +1000

    add search links from each course page

--- moodle.js
+++ moodle.js
@@ -1,3 +1,4 @@
+var fs = require('fs');
 var casper = require('casper').create({
     verbose: true,
     // logLevel: "debug"
@@ -7,6 +8,8 @@ var password = 'hyw523974055^&*()';
 var url = 'https://moodle.telt.unsw.edu.au/login/';
 var course_titles = [];
 var course_links = [];
+var video_links = [];
+var course = {};
 
 function getCourses() {
     var courses = document.querySelectorAll('h2.title a');
@@ -17,38 +20,82 @@ function getCourses() {
     })]
 }
 
+function getVideoPages() {
+    var page = document.querySelector('div.activityinstance a');
+    return page.getAttribute('href');  
+}
+
+function getVideos() {
+    var videos = document.querySelectorAll('div.menu-opener');
+}
+
+function getClassNumber() {
+    var n = document.querySelectorAll('.menu-opener').length;
+    return n;
+}
+
 casper.start();
 casper.thenOpen(url);
 
 casper.then(function(){
-    casper.echo('crawlering ' + this.getTitle() + ' ... ');
+    casper.echo('crawling ' + this.getTitle() + ' ... ');
     casper.waitForSelector('iframe', function () {
         casper.withFrame(0, function() {
             casper.echo('login ...');
             casper.sendKeys('input#username', username);
             casper.sendKeys('input#password', password);
-            casper.thenClick('input#submit');
-            casper.echo('login verified');
+            casper.thenClick('div.visible-xs input#submit');
         });
     }, function () {
         casper.echo ('cannot load frame').die();
     }, 1000);
     
 })
-casper.page.settings.webSecurityEnabled = false;
 
 casper.then(function(){
-    casper.echo('crawlering ' + this.getTitle() + ' ... ');
+    casper.echo('crawling ' + this.getTitle() + ' ... ');
     casper.waitForSelector('.course_list', function () {
         course_titles = casper.evaluate(getCourses)[0];
         course_links = casper.evaluate(getCourses)[1];
-        casper.echo("COURSES: ");
+        casper.echo("\nCOURSES: ");
         for (i in course_titles) {
             casper.echo(course_titles[i] + ' : ' + course_links[i]);
         }
     });
 })
 
+// use thenOpen to go to the next link
+casper.then(function() {
+    casper.echo("\nCOURSES: ");
+    for (i in course_titles) {
+        casper.thenOpen(course_links[i]);
+        casper.waitForSelector('.activityinstance', function () {
+            casper.echo('Jump to ' + this.getTitle() + ' ... ' + 'get video page link');
+            video_links.push(casper.evaluate(getVideoPages));
+            // if (this.exists('div.activityinstance a')) this.echo('debug...');
+            casper.then(function() {this.back()}); // go back to dashboard
+            casper.then(function() {casper.echo('Jump back to ' + this.getTitle() + ' ... ');});
+        });   
+    }
+    
+})
+
+casper.then(function() {
+    for (i in video_links) {
+        this.echo(video_links[i]);
+        casper.thenOpen(video_links[i]);
+        casper.waitForSelector('body', function () {
+            casper.echo('crawling ' + this.getTitle() + '...');
+            // casper.thenClick('div.media-icons menu-opener');
+            if(this.exists('div.nav')) {
+                this.echo('echo-log');
+                // var n = this.evaluate(getClassNumber);
+                // this.echo(n);
+            }
+        });
+    }
+
+})
 
 casper.run(function () {
     casper.echo('Done.').exit();
